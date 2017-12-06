2183e3008e09eff5433dbb37976589da43c6414b
EL SEIFY Ramy <elseify.ramy@gmail.com>
2017-12-06 17:05:44 +0100

    routing

--- WebinatorFront/src/app/app.module.ts
+++ WebinatorFront/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
