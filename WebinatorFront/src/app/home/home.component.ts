12df238422599a8bf214a4a75dc18450e7c3b9dd
Olivier <olivier@oliviers-mbp.home>
2017-12-19 21:18:34 +0100

    front modif

--- WebinatorFront/src/app/home/home.component.ts
+++ WebinatorFront/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { Planet } from '../planet';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  planets: Planet[] = [
+      { id: 1, ships: 16, event: 0 },
+      { id: 2, ships: 72, event: 0 }
+  ];
+
+  defaultUser: User = new User();
+
+  padding: { left: number, right: number, top: number, bottom: number }
+    = { left: 220, right: 220, top: 100, bottom: 0}
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.defaultUser.planets = this.planets;
   }
 
 }
