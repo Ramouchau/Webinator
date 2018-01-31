6a2278fba5bb5a9723f2edc61113ab1058cbaba1
EL SEIFY Ramy <elseify.ramy@gmail.com>
2018-01-31 10:40:25 +0100

    start game

--- src/app/_services/home.service.ts
+++ src/app/_services/home.service.ts
@@ -8,6 +8,8 @@ import {
 	RSocketListUserPlanetsOutputs,
 	RSocketAttackPlanetInputs,
 	RSocketAttackPlanetOutputs,
+	RSocketStartGameInputs,
+	RSocketStartGameOutputs,
 	APIError,
 	APISuccess
 } from '@etna-proj/webinator-server';
@@ -36,6 +38,7 @@ export class HomeService {
 				user.email = data.contents.user.email;
 				user.score = data.contents.user.score;
 				user.username = data.contents.user.username;
+				user.actionPointsCount = data.contents.user.actionPointsCount;
 				return cb(data.contents);
 			}
 		});
@@ -43,7 +46,6 @@ export class HomeService {
 
 	public getUserPlanets() {
 		return new Promise((resolve, reject) => {
-			const token = localStorage.getItem(loginToken);
 			this._rsoket.get<RSocketListUserPlanetsInputs, APIError | APISuccess<RSocketListUserPlanetsOutputs>>('list-user-planets', {}, (err, data) => {
 				if (err)
 					return reject(err);
@@ -51,6 +53,19 @@ export class HomeService {
 					return reject(data.error);
 				else
 					return resolve(data.contents.planets);
+			});
+		});
+	}
+
+	public startGame() {
+		return new Promise((resolve, reject) => {
+			this._rsoket.get<RSocketStartGameInputs, APIError | APISuccess<RSocketStartGameOutputs>>('start-game', {}, (err, data) => {
+				if (err)
+					return reject(err);
+				else if (data.error)
+					return reject(data.error);
+				else
+					return resolve(true);
 			});
 		});
 	}
